f9d740a951a7de1b341b3152c0d9bc9f36648be5
cnguye17 <cnguye67@msudenver.edu>
2021-06-13 20:23:25 -0600

    Foundations for Personal Website

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElements';
+
+import Video from '../../videos/COAST_Trim.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='COAST_Trim/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Calvin Nguyen</HeroH1>
+        <HeroP>
+          I'm a final-year student at Metropolitan State University of Denver majoring in Computer Science and minoring in Mathematics and Management, with aspirations to become a 
+          software engineer. 
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Contact Me! {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/Services/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Icon1 from '../../images/python.png';
+import Icon2 from '../../images/react.png';
+import Icon3 from '../../images/angular.png';
+import {
+  ServicesContainer,
+  ServicesH1,
+  ServicesWrapper,
+  ServicesCard,
+  ServicesIcon,
+  ServicesH2,
+  ServicesP
+} from './ServicesElements';
+
+const Services = () => {
+  return (
+    <ServicesContainer id='services'>
+      <ServicesH1>My Skills</ServicesH1>
+      <ServicesWrapper>
+        <ServicesCard>
+          <ServicesIcon src={Icon1} />
+          <ServicesH2>Python</ServicesH2>
+          <ServicesP>
+            Coded a span of projects in Python and and is my strongest coding language.
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon2} />
+          <ServicesH2>React Native</ServicesH2>
+          <ServicesP>
+            I developed both EESANA and this website using React Native. This framework is based of the use of javascript, HTML, and CSS .
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon3} />
+          <ServicesH2>Angular</ServicesH2>
+          <ServicesP>
+            I developed MSU Konnect using angular, implementing the MEAN Stack framework.
+          </ServicesP>
+        </ServicesCard>
+      </ServicesWrapper>
+    </ServicesContainer>
+  );
+};
+
+export default Services;
